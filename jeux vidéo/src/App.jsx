776043c5e54f58f7dbf0f06b91efa1799878c238
AntoineO-dev <antoine.oriol01@gmail.com>
2025-02-11 16:46:05 +0100

    fetch

--- jeux vidéo/src/App.jsx
+++ jeux vidéo/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
+  const [games, setGames] = useState([]);
 
+
+  const fetchGames = () => {
+    fetch("(https://rawg.io/api/gameshttps://rawg.io/api/games?key=5f1192bb6a474201934a847e7665a319")
+      .then(response => response.json())
+      .then(data => setGames(data.results))
+      .catch(error => console.error('Error fetching games:', error));
+  };
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
 
   return (
     <>
-     <h1>Jeux vidéos</h1>
+      <h1>Jeux vidéos</h1>
+      <ul>
+        {games.map(game => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </>
   )
 }
